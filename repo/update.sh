205d6928a3dfeecefc256a22956bc73096d12a6f
Kiran Patil <patilkiran08@gmail.com>
2018-03-04 12:21:45 -0600

    Added Packages

--- repo/update.sh
+++ repo/update.sh
@@ -1,5 +1,5 @@
 rm ./Packages
-./dpkg-scanpackages -m . /dev/null >Packages
+dpkg-scanpackages -m . /dev/null >Packages
 #./dpkg-scanpackages debs / > Packages
 rm ./Packages.bz2
 bzip2 -fks Packages
